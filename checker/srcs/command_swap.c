1916a489a8188f94e84eae45b134505dba7f7cf1
kiri0314 <t.kirihara0314@gmail.com>
2021-12-07 20:34:48 +0900

    [fix]ss,rr,rrrの修正

--- checker/srcs/command_swap.c
+++ checker/srcs/command_swap.c
@@ -7,11 +7,13 @@ void	swap(int *n1, int *n2, char *message)
 	tmp = *n1;
 	*n1 = *n2;
 	*n2 = tmp;
-	print_stdout(message);
+	if (ft_strncmp(message, "ss", 2))
+		print_stdout(message);
 }
 
 void	swap_ab(t_stack *stack_a, t_stack *stack_b)
 {
-	swap(&stack_a->num[stack_a->top], &stack_a->num[stack_a->top - 1], "sa");
-	swap(&stack_b->num[stack_b->top], &stack_b->num[stack_b->top - 1], "sb");
+	swap(&stack_a->num[stack_a->top], &stack_a->num[stack_a->top - 1], "ss");
+	swap(&stack_b->num[stack_b->top], &stack_b->num[stack_b->top - 1], "ss");
+	print_stdout("ss");
 }
