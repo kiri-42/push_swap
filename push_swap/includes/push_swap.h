db9a784c666b494101f993f885ff644786cb581a
kiri0314 <t.kirihara0314@gmail.com>
2021-12-08 18:04:26 +0900

    [fix]ヘッダーのプロトタイプ宣言の並びを変更

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 15:58:28 by tkirihar          #+#    #+#             */
-/*   Updated: 2021/12/08 17:22:04 by tkirihar         ###   ########.fr       */
+/*   Updated: 2021/12/08 17:30:21 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,11 @@ typedef struct s_stack
 	size_t	top;
 }	t_stack;
 
-/* init_stack */
-int		init_stack(t_stack *stack_a, t_stack *stack_b, int ac, char **av);
-
-/* print_message */
-void	print_stdout(char *message);
-void	print_stderr(void);
-
 /* check_arg */
 int		check_arg(int ac, char **av);
+
+/* init_stack */
+int		init_stack(t_stack *stack_a, t_stack *stack_b, int ac, char **av);
 
 /* sort */
 int		sort(int sort_size, t_stack *stack_a, t_stack *stack_b);
@@ -42,23 +38,6 @@ void	B_to_A(size_t sort_size, t_stack *stack_b, t_stack *stack_a);
 /* three_sort */
 int		three_sort(t_stack *stack);
 
-/* swap_operation */
-void	swap(int *n1, int *n2, char *message);
-void	swap_ab(t_stack *stack_a, t_stack *stack_b);
-
-/* push_operation */
-void	push(t_stack *push_stack, t_stack *pop_stack, char *message);
-
-/* rotate_operation */
-void	rotate(t_stack *stack, char *message);
-void	rrotate(t_stack *stack, char *message);
-void	rotate_ab(t_stack *stack_a, t_stack *stack_b);
-void	rrotate_ab(t_stack *stack_a, t_stack *stack_b);
-
-/* search_median */
-int		search_median(t_stack *stack, size_t sort_size, int *median);
-// void	quick_sort(int *stack, int left, int right);
-
 /* search_min */
 size_t	search_min(t_stack *stack);
 
@@ -67,5 +46,25 @@ void	A_to_B(size_t sort_size, t_stack *stack_a, t_stack *stack_b);
 
 /* B_to_A */
 void	B_to_A(size_t sort_size, t_stack *stack_b, t_stack *stack_a);
+
+/* print_message */
+void	print_stdout(char *message);
+void	print_stderr(void);
+
+/* search_median */
+int		search_median(t_stack *stack, size_t sort_size, int *median);
+
+/* command_swap */
+void	swap(int *n1, int *n2, char *message);
+void	swap_ab(t_stack *stack_a, t_stack *stack_b);
+
+/* command_push */
+void	push(t_stack *push_stack, t_stack *pop_stack, char *message);
+
+/* command_rotate */
+void	rotate(t_stack *stack, char *message);
+void	rrotate(t_stack *stack, char *message);
+void	rotate_ab(t_stack *stack_a, t_stack *stack_b);
+void	rrotate_ab(t_stack *stack_a, t_stack *stack_b);
 
 #endif
--- push_swap/srcs/sort.c
+++ push_swap/srcs/sort.c
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 15:58:21 by tkirihar          #+#    #+#             */
-/*   Updated: 2021/12/08 15:58:22 by tkirihar         ###   ########.fr       */
+/*   Updated: 2021/12/08 17:24:19 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,100 +49,6 @@ static int	medium_sort(t_stack *stack_a, t_stack *stack_b)
 		push(stack_a, stack_b, "pa");
 	}
 	return (0);
-}
-
-void	B_to_A(size_t sort_size, t_stack *stack_b, t_stack *stack_a)
-{
-	int		pivot;
-	size_t	count_rb;
-	size_t	count_pa;
-	size_t	i;
-
-	if (sort_size <= 1)
-	{
-		push(stack_a, stack_b, "pa");
-		return ;
-	}
-	if (sort_size == 2)
-	{
-		if (stack_b->num[stack_b->top] < stack_b->num[stack_b->top - 1])
-			swap(&stack_b->num[stack_b->top], &stack_b->num[stack_b->top - 1],
-				"sb");
-		push(stack_a, stack_b, "pa");
-		push(stack_a, stack_b, "pa");
-		return ;
-	}
-	if (search_median(stack_b, sort_size, &pivot))
-	{
-		print_stderr();
-		free(stack_a->num);
-		free(stack_b->num);
-		exit(1);
-	}
-	count_rb = 0;
-	count_pa = 0;
-	while (sort_size--)
-	{
-		if (stack_b->num[stack_b->top] < pivot)
-		{
-			rotate(stack_b, "rb");
-			count_rb++;
-		}
-		else
-		{
-			push(stack_a, stack_b, "pa");
-			count_pa++;
-		}
-	}
-	i = 1;
-	while (i <= count_rb)
-	{
-		rrotate(stack_b, "rrb");
-		i++;
-	}
-	A_to_B(count_pa, stack_a, stack_b);
-	B_to_A(count_rb, stack_b, stack_a);
-}
-
-void	A_to_B(size_t sort_size, t_stack *stack_a, t_stack *stack_b)
-{
-	int		pivot;
-	size_t	count_ra;
-	size_t	count_pb;
-	size_t	i;
-
-	if (sort_size <= 1)
-		return ;
-	if (search_median(stack_a, sort_size, &pivot))
-	{
-		print_stderr();
-		free(stack_a->num);
-		free(stack_b->num);
-		exit(1);
-	}
-	count_ra = 0;
-	count_pb = 0;
-	while (sort_size--)
-	{
-		if (stack_a->num[stack_a->top] > pivot)
-		{
-			rotate(stack_a, "ra");
-			count_ra++;
-		}
-		else
-		{
-			push(stack_b, stack_a, "pb");
-			count_pb++;
-		}
-	}
-	i = 1;
-	while (i <= count_ra)
-	{
-		rrotate(stack_a, "rra");
-		i++;
-	}
-	A_to_B(count_ra, stack_a, stack_b);
-	B_to_A(count_pb, stack_b, stack_a);
 }
 
 static void	long_sort(int sort_size, t_stack *stack_a, t_stack *stack_b)
